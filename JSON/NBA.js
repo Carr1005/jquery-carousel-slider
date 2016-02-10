6b3bc47b2dfa7035d357f1b88032552f0d455c65
Carr1005 <kaiboy6302@gmail.com>
2016-02-10 22:28:32 +0800

    First commit of Imitate_FB_Suggestion_Box

---
+++ JSON/NBA.js
@@ -0,0 +1,17 @@
+{	
+	"NBA":[
+    	{"team":"Cavaliers","city":"Cleveland"},
+    	{"team":"Bulls","city":"Chicago"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Heat","city":"Miami"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"},
+    	{"team":"Warriors","city":"Golden State"}
+]}
