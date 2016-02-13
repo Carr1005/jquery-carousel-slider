ca27c29c554eb38bb07250341605271529bf9aba
Carr1005 <kaiboy6302@gmail.com>
2016-02-13 20:56:33 +0800

    Using JSON file to generate content in li element

--- JSON/NBA.js
+++ JSON/NBA.js
@@ -1,17 +1,19 @@
 {	
-	"NBA":[
-    	{"team":"Cavaliers","city":"Cleveland"},
-    	{"team":"Bulls","city":"Chicago"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Heat","city":"Miami"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"},
-    	{"team":"Warriors","city":"Golden State"}
+	"NBA_teams":[
+    	{"team":"Cavaliers","city":"Cleveland","imgpath":"img/NBA/Cavaliers.jpg"},
+    	{"team":"Bulls","city":"Chicago","imgpath":"img/NBA/Bulls.jpg"},
+    	{"team":"Warriors","city":"Golden State","imgpath":"img/NBA/Warriors.jpg"},
+    	{"team":"Heat","city":"Miami","imgpath":"img/NBA/Heat.jpg"},
+    	{"team":"Thunder","city":"Oklahoma City","imgpath":"img/NBA/Thunder.jpg"},
+    	{"team":"Spurs","city":"San Antonio","imgpath":"img/NBA/Spurs.jpg"},
+    	{"team":"Rockets","city":"Houston","imgpath":"img/NBA/Rockets.jpg"},
+    	{"team":"Lakers","city":"Los Angeles","imgpath":"img/NBA/Lakers.jpg"},
+    	{"team":"Bucks","city":"Milwaukee","imgpath":"img/NBA/Bucks.jpg"},
+    	{"team":"Celtics","city":"Boston","imgpath":"img/NBA/Celtics.jpg"},
+    	{"team":"Hornets","city":"Charlotte","imgpath":"img/NBA/Hornets.jpg"},
+    	{"team":"Pistons","city":"Detroit","imgpath":"img/NBA/Pistons.jpg"},
+    	{"team":"Mavericks","city":"Dallas","imgpath":"img/NBA/Mavericks.jpg"},
+    	{"team":"Hawks","city":"Atlanta","imgpath":"img/NBA/Hawks.jpg"},
+        {"team":"Grizzlies","city":"Memphis","imgpath":"img/NBA/Grizzlies.jpg"},
+        {"team":"Raptors","city":"Toronto","imgpath":"img/NBA/Raptors.jpg"}
 ]}
